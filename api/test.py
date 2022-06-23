1f1f4d9285c44bb2b58a79fae7649320f4c6775b
zhouhuajian <zhouhuajian@email.com>
2022-06-24 02:42:43 +0800

    测试数据生成器 v0.2
    1. 增加表情测试数据的生成。

--- api/test.py
+++ api/test.py
@@ -14,7 +14,11 @@ bp = Blueprint('api_test', __name__)
 
 # 测试文本用的字符
 # TODO: 补充更多字符
-chars="0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz共决压争划列则光先阶那关再动军农会众传价件任全华产交论设许达过导并年当合各后名同向问安好如她江红级约场地在回团因多式存成观老机权收次有此百而米色西行至自乓乒乔丢买兴冰冲厌创刚刘刑兆亚匠防邪阳阴阵网劣企伞仰伐仿伏伙伤似伟伪伍休优协充亦访讽讲延芒芝巡州迈迁迅寺寻夺夹夸巩异庆庄帆师吃吊吓吉吗吐吸驰闭闯守宇宅妇奸妈妄岂岁屿尽壮扛扣扩扫托扬执池汗汤污纪纤圾尘尖忙孙字负贞毕轨死危爷戏灯灰考朵朴杀朽杂朱欢旬早旨曲肌臣虫耳齐肉舌羽舟竹页衣血羊份"
+chars = "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz共决压争划列则光先阶那关再动军农会众传价件任全华产交论设许达过导并年当合各后名同向问安好如她江红级约场地在回团因多式存成观老机权收次有此百而米色西行至自乓乒乔丢买兴冰冲厌创刚刘刑兆亚匠防邪阳阴阵网劣企伞仰伐仿伏伙伤似伟伪伍休优协充亦访讽讲延芒芝巡州迈迁迅寺寻夺夹夸巩异庆庄帆师吃吊吓吉吗吐吸驰闭闯守宇宅妇奸妈妄岂岁屿尽壮扛扣扩扫托扬执池汗汤污纪纤圾尘尖忙孙字负贞毕轨死危爷戏灯灰考朵朴杀朽杂朱欢旬早旨曲肌臣虫耳齐肉舌羽舟竹页衣血羊份"
+
+# 测试表情用的表情
+# TODO: 补充更多表情
+emojis = "😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀"
 
 @bp.route('/api/test/text', methods=['POST'])
 def text():
@@ -22,6 +26,17 @@ def text():
     size = int(request.form['size'])
     data = ''.join(random.sample(chars, size))
     db.session.add(TestData(type="text", size=size, data=data))
+    db.session.commit()
+    # 回到测试数据页面
+    return redirect('/test/data')
+
+
+@bp.route('/api/test/emoji', methods=['POST'])
+def emoji():
+    """生成测试表情"""
+    size = int(request.form['size'])
+    data = ''.join(random.sample(emojis, size))
+    db.session.add(TestData(type="emoji", size=size, data=data))
     db.session.commit()
     # 回到测试数据页面
     return redirect('/test/data')
